9de4ab77831739dee17488c7ae6d5814893d0b82
Gulnaz Ahmad <130208290+killua0393@users.noreply.github.com>
2024-08-31 15:45:36 +0530

    Add files via upload

---
+++ 4.3 EJS Partials/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+ 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static(__dirname + "/public"));
+
+app.set('view engine', 'ejs');
+
+
+app.get("/", (req, res) => {
+  
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  
+  res.render("contact");
+});
+
+
+/* Write your code here:
+Step 1: Render the home page "/" index.ejs
+Step 2: Make sure that static files are linked to and the CSS shows up.
+Step 3: Add the routes to handle the render of the about and contact pages.
+  Hint: Check the nav bar in the header.ejs to see the button hrefs
+Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
